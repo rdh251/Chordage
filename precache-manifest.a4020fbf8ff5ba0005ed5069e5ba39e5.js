8201492da9d06a36939557de714ed1463e8980fd
Ross <RossHall251@outlook.com>
2019-04-18 13:55:52 -0400

    Updates

--- precache-manifest.a4020fbf8ff5ba0005ed5069e5ba39e5.js
+++ precache-manifest.a4020fbf8ff5ba0005ed5069e5ba39e5.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Chordage/static/js/runtime~main.473deb00.js"
   },
   {
-    "revision": "ac793a4d74c58e4d583f",
-    "url": "/Chordage/static/js/main.ac793a4d.chunk.js"
+    "revision": "533ea57f11bbf06e3758",
+    "url": "/Chordage/static/js/main.533ea57f.chunk.js"
   },
   {
     "revision": "12eb6e5f2710ecaf2d31",
     "url": "/Chordage/static/js/2.12eb6e5f.chunk.js"
   },
   {
-    "revision": "ac793a4d74c58e4d583f",
+    "revision": "533ea57f11bbf06e3758",
     "url": "/Chordage/static/css/main.68c8b090.chunk.css"
   },
   {
-    "revision": "de8761e77400b0b6bcd7f7b30c20d24c",
+    "revision": "1417ad79aa7a3c5c271529bac76775bc",
     "url": "/Chordage/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Chordage/precache-manifest.c517287b69930228bf6edcbac5b9ada3.js"
+  "/Chordage/precache-manifest.a4020fbf8ff5ba0005ed5069e5ba39e5.js"
 );
 
 workbox.clientsClaim();
