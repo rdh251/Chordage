6144703459bcc6025788a355fb15db12a924f7a3
Ross <RossHall251@outlook.com>
2019-05-02 17:41:49 -0400

    Updates

--- precache-manifest.48ce3aef52d76aa5dc76be546b5a38ea.js
+++ precache-manifest.48ce3aef52d76aa5dc76be546b5a38ea.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Chordage/static/js/runtime~main.473deb00.js"
   },
   {
-    "revision": "8b3fe6460c0db6564a23",
-    "url": "/Chordage/static/js/main.8b3fe646.chunk.js"
+    "revision": "047c629d1f8f8d78c9ab",
+    "url": "/Chordage/static/js/main.047c629d.chunk.js"
   },
   {
     "revision": "12eb6e5f2710ecaf2d31",
     "url": "/Chordage/static/js/2.12eb6e5f.chunk.js"
   },
   {
-    "revision": "8b3fe6460c0db6564a23",
-    "url": "/Chordage/static/css/main.ec4adcb3.chunk.css"
+    "revision": "047c629d1f8f8d78c9ab",
+    "url": "/Chordage/static/css/main.695df4c7.chunk.css"
   },
   {
-    "revision": "67e410d7e3cb51af8cc3613bd51fdd9e",
+    "revision": "7fb021b5777833835bfff0ec33a5aa63",
     "url": "/Chordage/index.html"
   }
 ];
